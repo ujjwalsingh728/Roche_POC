334a7e02569e6e0f167002a68f3c55e450638b12
Atef Alrwashdeh <atef.alrwashdeh@gmail.com>
2022-07-21 22:56:27 -0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDragExit,
+  CdkDragHandle,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +14,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  todo:string[] = [ '99985', 'Go home', 'Fall asleep','Get up', 'Brush teeth',
+    'Cencer reasearch', 'Check e-mail', 'Walk dog','be human'];
+
+  done:string[] = [];
+
+  compareButtonShow: boolean = false;
+
+  drop(event: CdkDragDrop<string[]>) {
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else  {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+
+  deleteSelected(i: number) {
+
+      console.log(i);
+    this.todo.push(...this.done.splice(i, 1));
+
+
+  }
+  checkNumberInDoneList():boolean{
+    return true;
+  }
+
+  toggle():boolean{
+    if(this.done.length > 1){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  changeColor() :string{
+    if(this.done.length > 1){
+      return 'purple';
+    }else{
+      return 'gray';
+    }
+  }
+
+  removeAllElement() {
+    this.todo.push(...this.done.splice(0,this.done.length));
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButton, MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DragDropModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
